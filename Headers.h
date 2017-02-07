9b64083b4ae78d23be618e7fbbf8be3599f472fc
Sticktron <sticktron@hotmail.com>
2017-02-07 09:33:39 -0500

    cleaned out headers file

--- Headers.h
+++ Headers.h
@@ -2,7 +2,6 @@
 // Private APIs
 //
 
-
 @interface UIApplication (Private)
 - (void)terminateWithSuccess;
 @end
@@ -11,34 +10,4 @@
 @end
 
 @interface CKUIThemeDark : CKUITheme
-@end
-
-@interface CKUIBehavior : NSObject
-- (id)theme;
-@end
-
-@interface CKUIBehaviorPhone : CKUIBehavior
-@end
-
-@interface CKUIBehaviorPad : CKUIBehavior
-@end
-
-@interface CKAvatarNavigationBar : UINavigationBar
-- (void)_setBarStyle:(int)style;
-@end
-
-@interface CKAvatarContactNameCollectionReusableView : UICollectionReusableView
-- (void)setStyle:(int)style;
-@end
-
-@interface CKAvatarTitleCollectionReusableView : UICollectionReusableView
-- (void)setStyle:(int)arg1;
-@end
-
-@interface CKNavigationBarCanvasView : UIView
-- (void)setTitleView:(id)titleView;
-@end
-
-@interface CKDetailsContactsTableViewCell : UITableViewCell
-- (UILabel *)nameLabel;
 @end
