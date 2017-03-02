5d6affde46937e72465cb8d028f49c2d7bddd35b
Sticktron <sticktron@hotmail.com>
2017-03-02 15:43:11 -0500

    Added NightModeControl to headers, removed some unused stuff

--- DarkMessages.h
+++ DarkMessages.h
@@ -14,15 +14,12 @@
 #define kSettingsChangedNotification 	CFSTR("com.sticktron.darkmessages.settingschanged")
 #define kRelaunchMobileSMSNotification 	CFSTR("com.sticktron.darkmessages.relaunchmobilesms")
 
+
 // Private APIs
 
 @interface UIApplication (DM)
 - (BOOL)isSuspended;
 - (void)terminateWithSuccess;
-@end
-
-@interface UIWindow (DM)
-+ (id)keyWindow;
 @end
 
 @interface CKUITheme : NSObject
@@ -33,34 +30,20 @@
 - (UIColor *)entryFieldDarkStyleButtonColor;
 @end
 
+@interface NightModeControl : NSObject // from CoreBrightness
+- (void)enableBlueLightReduction:(BOOL)arg1 withOption:(int)arg2;
+@end
+
 @interface AXBackBoardServer : NSObject
 + (id)server;
 - (BOOL)blueLightStatusEnabled;
 @end
 
-@interface SpringBoard : NSObject
-- (BOOL)launchApplicationWithIdentifier:(id)arg1 suspended:(BOOL)arg2;
-- (void)_simulateHomeButtonPress;
-- (void)_simulateHomeButtonPressWithCompletion:(id /*block*/)arg1;
-- (id)_accessibilityFrontMostApplication;
-@end
-
 @interface SBApplication : NSObject
-- (id)bundleIdentifier;
 @end
 
 @interface SBApplicationController : NSObject
 + (id)sharedInstance;
 - (id)applicationWithBundleIdentifier:(id)arg1;
 - (void)applicationService:(id)arg1 suspendApplicationWithBundleIdentifier:(id)arg2;
-- (void)applicationService:(id)arg1 setNextWakeDate:(id)arg2 forBundleIdentifier:(id)arg3;
-@end
-
-@interface SBUIController : NSObject
-+ (id)sharedInstanceIfExists;
-- (void)activateApplication:(id)arg1;
-@end
-
-@interface FBUIApplicationService : NSObject
-+ (id)sharedInstance;
 @end
